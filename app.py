7843cd38a0edbdb363d830a9f9b9e09df2351331
PowerLay <1854pro@gmail.com>
2022-01-06 14:00:38 +0300

    Add hotreload

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return 'Moe Flask приложение в контейнере Docker.'
+    return "<p>Hello, Worlds!</p>"
 
 
 if __name__ == '__main__':
